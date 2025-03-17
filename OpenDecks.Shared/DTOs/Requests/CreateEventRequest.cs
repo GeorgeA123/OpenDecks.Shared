810520ceb5c9d6e876ba2abbc3403e90aae8ce68
George <george.i.alexander@live.co.uk>
2025-03-17 20:15:20 +0000

    Init

---
+++ OpenDecks.Shared/DTOs/Requests/CreateEventRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDecks.Shared.DTOs.Requests
+{
+    public class CreateEventRequest
+    {
+        public string Name { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public DateTime ApplicationDeadline { get; set; }
+    }
+}
---
+++ OpenDecks.Shared/DTOs/Responses/EventResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDecks.Shared.DTOs.Responses
+{
+    public class EventResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime EventDate { get; set; }
+        public DateTime ApplicationDeadline { get; set; }
+        public int TotalApplications { get; set; }
+    }
+}
