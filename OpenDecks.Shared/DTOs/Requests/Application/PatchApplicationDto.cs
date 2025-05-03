0ee4bcc2ebc18be2212e704c7e8144771a1d9a15
George <george.i.alexander@live.co.uk>
2025-05-03 20:51:41 +0100

    Application Selection

---
+++ OpenDecks.Shared/DTOs/Requests/Application/PatchApplicationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenDecks.Shared.DTOs.Requests.Application
+{
+    public class PatchApplicationDto
+    {
+        public string Status { get; set; } = string.Empty;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
---
+++ OpenDecks.Shared/Validators/Application/PatchApplicationDtoValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using OpenDecks.Shared.DTOs.Requests.Application;
+using OpenDecks.Shared.Enums;
+
+namespace OpenDecks.Shared.Validators.Application
+{
+    public class PatchApplicationDtoValidator : AbstractValidator<PatchApplicationDto>
+    {
+        public PatchApplicationDtoValidator()
+        {
+            RuleFor(a => a.Status)
+                .Must(BeValidApplicationStatus)
+                .When(a => !string.IsNullOrEmpty(a.Status))
+                .WithMessage("Invalid application status");
+
+        }
+
+        private bool BeValidApplicationStatus(string status)
+        {
+            return Enum.TryParse<ApplicationStatus>(status, true, out _);
+        }
+    }
+}
