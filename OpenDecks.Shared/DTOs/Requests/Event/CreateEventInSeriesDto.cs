920feb2db4c18a63bf66c188ce4956ea9c5fa985
George <george.i.alexander@live.co.uk>
2025-05-08 17:30:00 +0100

    Remove unneeded imports

--- OpenDecks.Shared/DTOs/Requests/Event/CreateEventInSeriesDto.cs
+++ OpenDecks.Shared/DTOs/Requests/Event/CreateEventInSeriesDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OpenDecks.Shared.DTOs.Requests.Event
+﻿namespace OpenDecks.Shared.DTOs.Requests.Event
 {
     public class CreateEventInSeriesDto
     {
