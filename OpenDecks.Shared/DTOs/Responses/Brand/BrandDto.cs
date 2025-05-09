6e748f35b56f51de2ba069f70dac41025d64b540
George <george.i.alexander@live.co.uk>
2025-05-09 12:16:59 +0100

    Updating

--- OpenDecks.Shared/DTOs/Responses/Brand/BrandDto.cs
+++ OpenDecks.Shared/DTOs/Responses/Brand/BrandDto.cs
@@ -9,6 +9,7 @@ namespace OpenDecks.Shared.DTOs.Responses.Brand
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string LogoUrl { get; set; } = string.Empty;
+
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public List<EventDto> Events { get; set; } = new List<EventDto>();
